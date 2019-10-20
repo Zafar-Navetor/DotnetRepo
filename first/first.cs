dc8f1d6f61c0dcbd65a875796c0f57fc1d84dca9
zafar <zfrahmad@gmail.com>
2019-10-20 17:52:13 +0500

    My first dot Net Program

---
+++ first/first.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace first
+{
+    class first
+    {
+        int x = 100;
+        static void Main(string[] args)
+        {
+            first f1 = new first();
+            first f2 = f1;
+            Console.WriteLine(f1.x + " " + f2.x);
+            Console.ReadLine();
+        }
+    }
+}
